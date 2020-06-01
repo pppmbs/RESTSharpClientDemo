7b42919308261db75a6f7380e3e85ca4014c79be
pppmbs <pppmbs@gmail.com>
2020-06-01 14:36:40 -0500

    Add project files.

---
+++ RESTSharpClientDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+
+namespace RESTSharpClientDemo
+{
+    class Program
+    {
+        private static RestClient client = new
+        RestClient("http://localhost:5001/api/");
+        static void Main(string[] args)
+        {
+            RestRequest request = new RestRequest("Default",
+            Method.GET);
+            IRestResponse<List<string>> response =
+            client.Execute<List<string>>(request);
+            Console.ReadKey();
+        }
+    }
+}
